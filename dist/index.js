df6292de29cadc62e6e97b67eca223cb8f6124c7
SeddonShen <283481855@qq.com>
2023-02-27 16:38:07 +0800

    v0.0.2

--- dist/index.js
+++ dist/index.js
@@ -4113,16 +4113,6 @@ const exec = __nccwpck_require__(525);
 
 async function run() {
   try {
-    // // Validate action
-    // if (
-    //   !core.getInput("action")
-    // ) {
-    //   throw new Error("action missing or in the wrong format.");
-    // }
-
-    // const action = core.getInput("action");
-    // console.log(`Action: ${action}`);
-    // Execute build.sh
     await exec.exec(`bash ${__dirname}/../build.sh`);
   } catch (error) {
     core.setFailed(error.message);
